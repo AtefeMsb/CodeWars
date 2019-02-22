eb0f3e786660dae04245dff607ed719148a30e69
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-02-21 17:28:37 -0800

    push test.js

--- Javascript/test.js
+++ Javascript/test.js
@@ -9,14 +9,22 @@ class Mammel {
     }
 }
 
-class dog extends Mammel {
+class Dog extends Mammel {
     constructor() {
         super("WOOOOOOOOFF");
     }
 }
 
-let mycat = new Mammel("woof");
+let mycat = new Dog("woof");
+mycat.sound = "new wooooooooof"
 let x = mycat.talk()
 x
+let type = typeof Dog;
+type
+let y = Dog.prototype.talk
+y
+let z = Dog.prototype.sound
+z
+
 
 
